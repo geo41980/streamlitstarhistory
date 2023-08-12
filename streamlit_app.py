340ee855ad80321d36abc011ea8214bdbc9b6d59
Georg <141832610+geo41980@users.noreply.github.com>
2023-08-12 14:34:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,3 @@
-import streamlit as st
-import pandas as pd #if you will
-import gspread
-from google.oauth2 import service_account
-
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
-    ],
-)
-conn = connect(credentials=credentials)
-client=gspread.authorize(credentials)
-
-sheet_id = '1K5_Rq3HQkIvtrbLl3VQqei1SVPjMnkxO8Hj64VtDPMc'
-csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
-database_df = pd.read_csv(csv_url, on_bad_lines='skip')
-
-database_df = database_df.astype(str)
-sheet_url = st.secrets["private_gsheets_url"] #this information should be included in streamlit secret
-sheet = client.open_by_url(sheet_url).sheet1
-sheet.update([database_df.columns.values.tolist()] + database_df.values.tolist())
-st.success('Data has been written to Google Sheets')
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('Tutorial Louis-985b5d882f76.json', scope) #Change to your downloaded JSON file name 
+client = gspread.authorize(creds)
