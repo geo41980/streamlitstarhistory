cc63c1a1e2283ab697cd2aedaad167e2cfec3678
Georg <141832610+geo41980@users.noreply.github.com>
2023-08-12 11:58:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 # Sheets-URL aus den Geheimnissen lesen
 sheets_url = st.secrets["public_gsheets_url"]
 
+st.write("Hallo Georg")
 # Daten aus der Google Sheet abrufen
 #df = pd.read_csv(sheets_url)
 
