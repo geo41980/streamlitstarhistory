7298c7a5a88500ce385aadef067c7495de241dbb
Georg <141832610+geo41980@users.noreply.github.com>
2023-08-12 16:43:53 +0200

    Update analysis-webapp.py

--- analysis-webapp.py
+++ analysis-webapp.py
@@ -1,42 +1,22 @@
-import streamlit as st 
-import streamlit.components.v1 as stc 
-import pandas as pd 
-import pygwalker as pyg 
-
-# Page Configuration
-st.set_page_config(page_title="StWalker App",layout="wide")
-
-# Load Data Fxn
-def load_data(data):
-    return pd.read_csv(data)
-
-def main():
-    st.title("Streamlit PyGWalker App")
-
-    menu = ["Home", "About"]
-    choice = st.sidebar.selectbox("Menu", menu)
-
-    if choice == "Home":
-        st.subheader("Home")
-        # Form
-        with st.form("upload_form"):
-            data_file = st.file_uploader("Upload a CSV File",type=["csv","txt"])
-            submitted = st.form_submit_button("Submit")
-
-        if submitted:
-            df = load_data(data_file)
-            st.dataframe(df)
-            # Visualize
-            pyg_html = pyg.walk(df,return_html=True)
-            # Render with components
-            stc.html(pyg_html,scrolling=True,height=1000)
-
-        
-        
-    
-    else:
-        st.subheader("About")
-
-    
-if __name__ == "__main__":
-    main()
+import pygwalker as pyg
+import pandas as pd
+import streamlit.components.v1 as components
+import streamlit as st
+ 
+# Breite der Streamlit-Seite anpassen
+st.set_page_config(
+    page_title="Pygwalker in Streamlit verwenden",
+    layout="wide"
+)
+ 
+# Titel hinzufügen
+st.title("Pygwalker in Streamlit verwenden")
+ 
+# Daten importieren
+df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
+ 
+# HTML mit Pygwalker generieren
+pyg_html = pyg.walk(df, return_html=True)
+ 
+# HTML in die Streamlit-Anwendung einbetten
+components.html(pyg_html, height=1000, scrolling=True)
