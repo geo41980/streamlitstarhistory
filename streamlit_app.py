179ea896cec0b64e30038a78d08cad6b74cf2bc5
Georg <141832610+geo41980@users.noreply.github.com>
2023-08-12 14:41:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('Tutorial Louis-985b5d882f76.json', scope) #Change to your downloaded JSON file name 
+creds = ServiceAccountCredentials.from_json_keyfile_name('secure-air-394815-6ca60e653ca3.json', scope) #Change to your downloaded JSON file name 
 client = gspread.authorize(creds)
