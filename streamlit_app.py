f54b28397c00e66972a76ba35ee8e8be795e1a4f
Georg <141832610+geo41980@users.noreply.github.com>
2023-08-12 11:20:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,11 @@
-# streamlit_app.py
-
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-# Read in data from the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
+# Sheets-URL aus den Geheimnissen lesen
+sheets_url = st.secrets["public_gsheets_url"]
 
-df = load_data(st.secrets["public_gsheets_url"])
+# Daten aus der Google Sheet abrufen
+df = pd.read_csv(sheets_url)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+# Daten in Streamlit anzeigen
+st.dataframe(df)
